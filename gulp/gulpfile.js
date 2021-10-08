a962f3e52df66760c5b511d3f4d239333cb8386a
mavrus76 <mav76rus@gmail.com>
2021-10-08 19:16:21 +0300

    update

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,63 +1,80 @@
-var {
+let {
   src,
   dest,
   series,
   watch
 } = require('gulp')
-var concat = require('gulp-concat')
-var htmlMin = require('gulp-htmlmin')
-var autoprefixes = require('gulp-autoprefixer')
-var cleanCSS = require('gulp-clean-css')
-var svgSprite = require('gulp-svg-sprite')
-var image = require('gulp-image')
-var babel = require('gulp-babel')
-var uglify = require('gulp-uglify-es').default
-var notify = require('gulp-notify')
-var sourcemaps = require('gulp-sourcemaps')
-var del = require('del')
-var browserSync = require('browser-sync').create()
+let concat = require('gulp-concat')
+let htmlMin = require('gulp-htmlmin')
+let autoprefixes = require('gulp-autoprefixer')
+let sass = require('gulp-sass')(require('sass'))
+let cleanCSS = require('gulp-clean-css')
+let webp = require('gulp-webp')
+let svgSprite = require('gulp-svg-sprite')
+let image = require('gulp-image')
+let babel = require('gulp-babel')
+let uglify = require('gulp-uglify-es').default
+let notify = require('gulp-notify')
+let sourcemaps = require('gulp-sourcemaps')
+let del = require('del')
+let browserSync = require('browser-sync').create()
 
-var cleanDev = () => {
-  return del(['dev'])
+let clean = () => {
+  exports.clean = clean
+  return del([
+    'build',
+    'dev'
+  ])
 }
 
-var clean = () => {
-  return del(['build'])
+let resources = () => {
+  return src('src/libs/**')
+    .pipe(dest('build/libs'))
+    .pipe(dest('dev/libs'))
 }
 
-var resources = () => {
-  return src('src/resources/**')
-    .pipe(dest('build'))
-    .pipe(dest('dev'))
+let fonts = () => {
+  return src('src/fonts/**')
+    .pipe(dest('build/fonts'))
+    .pipe(dest('dev/fonts'))
 }
 
-var stylesDev = () => {
-  return src('src/styles/**/*.css')
+let scss = () => {
+  return src('src/css/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(concat('main.css'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixes({
+      cascade: false,
+      grid: true,
+    }))
     .pipe(sourcemaps.write())
-    .pipe(dest('dev'))
+    .pipe(dest('src/css'))
 }
 
-var styles = () => {
-  return src('src/styles/**/*.css')
-    .pipe(concat('main.css'))
-    .pipe(autoprefixes({
-      cascade: false
-    }))
+let stylesDev = () => {
+  return src('src/css/**/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(concat('style.css'))
+    .pipe(sourcemaps.write())
+    .pipe(dest('dev/css'))
+}
+
+let styles = () => {
+  return src('src/css/**/*.css')
+    .pipe(concat('style.css'))
     .pipe(cleanCSS({
       level: 2
     }))
-    .pipe(dest('build'))
+    .pipe(dest('build/css'))
     .pipe(browserSync.stream())
 }
 
-var htmlMinifyDev = () => {
+let htmlMinifyDev = () => {
   return src('src/**/*.html')
     .pipe(dest('dev'))
 }
 
-var htmlMinify = () => {
+let htmlMinify = () => {
   return src('src/**/*.html')
     .pipe(htmlMin({
       collapseWhitespace: true,
@@ -66,8 +83,8 @@ var htmlMinify = () => {
     .pipe(browserSync.stream())
 }
 
-var svgSprites = () => {
-  return src('src/images/svg/**/*.svg')
+let svgSprites = () => {
+  return src('src/img/svg/**/*.svg')
     .pipe(svgSprite({
       mode: {
         stack: {
@@ -75,38 +92,32 @@ var svgSprites = () => {
         }
       }
     }))
-    .pipe(dest('build/images'))
-    .pipe(dest('dev/images'))
+    .pipe(dest('build/img'))
+    .pipe(dest('dev/img'))
 }
 
-var scriptsDev = () => {
-  return src([
-      'src/js/components/**/*.js',
-      'src/js/main.js'
-    ])
+let scriptsDev = () => {
+  return src('src/js/**/*.js')
     .pipe(sourcemaps.init())
-    .pipe(concat('app.js'))
+    .pipe(concat('script.js'))
     .pipe(sourcemaps.write())
-    .pipe(dest('dev'))
+    .pipe(dest('dev/js'))
 }
 
-var scripts = () => {
-  return src([
-      'src/js/components/**/*.js',
-      'src/js/main.js'
-    ])
+let scripts = () => {
+  return src('src/js/**/*.js')
     .pipe(babel({
       presets: ['@babel/env']
     }))
-    .pipe(concat('app.js'))
+    .pipe(concat('script.js'))
     .pipe(uglify({
       toplevel: true
     }).on('error', notify.onError()))
-    .pipe(dest('build'))
+    .pipe(dest('build/js'))
     .pipe(browserSync.stream())
 }
 
-var watchFiles = () => {
+let watchFiles = () => {
   browserSync.init({
     server: {
       baseDir: 'build'
@@ -114,28 +125,50 @@ var watchFiles = () => {
   })
 }
 
-var images = () => {
+let exportWebp = () => {
+  return src('src/img/**/*.{jpg,jpeg,png}')
+    .pipe(webp({
+      lossless: true
+    }))
+  //   .pipe(webp({
+  //     quality: 80,
+  //     preset: 'photo',
+  //     method: 6
+  // }))
+    .pipe(dest('build/img'))
+    .pipe(dest('dev/img'))
+}
+
+let images = () => {
   return src([
-      'src/images/**/*.jpg',
-      'src/images/**/*.png',
-      'src/images/*.svg',
-      'src/images/**/*.jpeg',
+      'src/img/**/*.{jpg,jpeg,png}',
+      'src/img/*.svg',
+      'src/img/*.ico'
     ])
     .pipe(image())
-    .pipe(dest('build/images'))
-    .pipe(dest('dev/images'))
+    .pipe(dest('build/img'))
+    .pipe(dest('dev/img'))
 }
 
 watch('src/**/*.html', htmlMinify)
 watch('src/**/*.html', htmlMinifyDev)
-watch('src/styles/**/*.css', styles)
-watch('src/styles/**/*.css', stylesDev)
-watch('src/images/svg/**/*.svg', svgSprites)
+watch('src/css/**/*.scss', scss)
+watch('src/css/**/*.css', styles)
+watch('src/css/**/*.css', stylesDev)
+watch('src/img/svg/**/*.svg', svgSprites)
 watch('src/js/**/*.js', scripts)
 watch('src/js/**/*.js', scriptsDev)
 watch('src/resources/**', resources)
+watch('src/img/**/*.{jpg,jpeg,png}', exportWebp)
+watch([
+  'src/img/**/*.{jpg,jpeg,png}',
+  'src/img/*.svg',
+  'src/img/*.ico'
+], images)
 
+exports.clean = clean
 exports.styles = styles
 exports.scripts = scripts
+exports.scss = scss
 exports.htmlMinify = htmlMinify
-exports.default = series(cleanDev, clean, resources, htmlMinifyDev, htmlMinify, scriptsDev, scripts, stylesDev, styles, images, svgSprites, watchFiles)
+exports.default = series(clean, resources, fonts, scss, htmlMinifyDev, htmlMinify, scriptsDev, scripts, stylesDev, styles, exportWebp, images, svgSprites, watchFiles)
