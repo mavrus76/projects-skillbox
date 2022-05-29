bc3e7b1dc54d8ceebb0977fad69a44e05e358ad7
Maxim Vysotskiy <75042447+mavrus76@users.noreply.github.com>
2022-05-29 11:17:44 +0300

    was updated homework 3.4

---
+++ first-vue-project/src/components/ProductList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="catalog__list">
+    <ProductItem
+      :products="products"
+      v-for="(product, index) in products"
+      :key="index"
+      :product="product"
+    ></ProductItem>
+  </ul>
+</template>
+
+<script>
+import ProductItem from './ProductItem.vue';
+
+export default {
+  components: { ProductItem },
+  props: ['products', 'product'],
+};
+</script>
